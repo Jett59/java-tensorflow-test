17313372baefb1ac5ef918973791bfc2d0cdf874
Jett Thompson <jett.thompson.59@gmail.com>
2022-04-10 20:06:49 +1000

    made the code create a graph with maths on it

--- src/main/java/app/cleancode/Entrypoint.java
+++ src/main/java/app/cleancode/Entrypoint.java
@@ -1,11 +1,27 @@
 package app.cleancode;
 
 import org.tensorflow.Graph;
-import org.tensorflow.Operation;
-import org.tensorflow.proto.framework.DataType;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.op.Ops;
+import org.tensorflow.op.core.Placeholder;
+import org.tensorflow.op.math.Mul;
+import org.tensorflow.op.math.Square;
+import org.tensorflow.types.TFloat64;
 
 public class Entrypoint {
     public static void main(String[] args) {
-        
+        try (Graph graph = new Graph()) {
+            Ops ops = Ops.create(graph);
+            Placeholder<TFloat64> x = ops.placeholder(TFloat64.class);
+            Mul<TFloat64> twoX = ops.math.mul(ops.constant(2.0), x);
+            Square<TFloat64> x2 = ops.math.square(twoX);
+
+            try (Session session = new Session(graph)) {
+                Tensor result =
+                        session.runner().feed(x, TFloat64.scalarOf(2.25)).fetch(x2).run().get(0);
+                System.out.println(((TFloat64) result).getDouble());
+            }
+        }
     }
 }

d4a6a21613aa14effa467a34347927270bebfbcb
Jett Thompson <jett.thompson.59@gmail.com>
2022-04-10 16:09:36 +1000

    added tensorflow dependencies

---
+++ src/main/java/app/cleancode/Entrypoint.java
@@ -0,0 +1,11 @@
+package app.cleancode;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Operation;
+import org.tensorflow.proto.framework.DataType;
+
+public class Entrypoint {
+    public static void main(String[] args) {
+        
+    }
+}
